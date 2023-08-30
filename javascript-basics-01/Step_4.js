25cdc0a3fee2fbf00fb31daaf4a62b443f364f15
moussakaram <moussakaram2@gmail.com>
2023-08-30 16:59:51 +0300

    Step_4

---
+++ javascript-basics-01/Step_4.js
@@ -0,0 +1,6 @@
+var fname=prompt("Enter your name please ");
+var surname=prompt("Enter your last name please ");
+var city=prompt("Enter your city name please");
+
+var message = "Name: "+fname+"\n Family name: "+surname+ "\nCity name:"+city;
+alert(message);
