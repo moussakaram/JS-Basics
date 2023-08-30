bd08068b177b8c713369e74c5806354b0f9c262d
moussakaram <moussakaram2@gmail.com>
2023-08-31 00:56:01 +0300

    Step_5

---
+++ javascript-basics-01/Step_5.js
@@ -0,0 +1,11 @@
+/*let firstNumber =parseFloat(document.getElementById("first_number").value);
+let secondNumber =parseFloat(document.getElementById("second_number").value);
+let results= firstNumber*secondNumber;
+function myFunction(){
+let message="the result of multiplication = "+results;
+alert(message);} */
+
+let first_number=prompt("Enter the firs number please");
+let second_number=prompt("Enter the second number please");
+let result=first_number*second_number;
+alert("the multipleof first number and second number= "+result);
