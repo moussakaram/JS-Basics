879bdce4b2914ac37794863854ab6fe7fa6e3aeb
moussakaram <moussakaram2@gmail.com>
2023-08-31 15:32:34 +0300

    Step_7

---
+++ javascript-basics-01/Step_7.js
@@ -0,0 +1,12 @@
+
+function myFunction(){
+    var a=document.getElementById("shoe_size").value;
+    var b=document.getElementById("year").value;
+    let mult=a*2;
+    let add=mult+5;
+    let multi=add*50;
+    let sub=multi-b;
+    let sum=sub+1766;
+     alert (sum);
+}
+
