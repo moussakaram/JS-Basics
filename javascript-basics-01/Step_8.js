7629bf37d380e207b37a005e45e694aeccfd457e
moussakaram <moussakaram2@gmail.com>
2023-08-31 16:06:53 +0300

    Step_8

---
+++ javascript-basics-01/Step_8.js
@@ -0,0 +1,10 @@
+function myFunction(){
+    let age=document.getElementById("age").value;
+    if(age>18){
+        alert ("you are over 18");
+     }else{
+        alert("you are under 18");
+     }
+ 
+
+}
