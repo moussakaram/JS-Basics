22992c66f5b155b71730684ce0806693656a08f2
moussakaram <moussakaram2@gmail.com>
2023-08-31 18:37:51 +0300

    first commit Step2 js

---
+++ javascript-basics-02/step 2/Step2.js
@@ -0,0 +1,5 @@
+const input = document.querySelector('#name');
+input.addEventListener('blur', function() {
+    const dialogMessage = "Thank you for participating!";
+    alert(dialogMessage);
+  });
