355bc82b1f18335c7d1ca8ee900c358724d7d22e
moussakaram <moussakaram2@gmail.com>
2023-09-01 12:49:44 +0300

    step 3 JS3

---
+++ javascript-basics-03/step 3/Step3.js
@@ -0,0 +1,24 @@
+const green1=document.querySelector(".green");
+const red1=document.querySelector(".red");
+const blue1=document.querySelector(".blue");
+const text1=document.getElementById("#text")
+
+
+green1.addEventListener("click",function(color){
+   
+    text.style.color = "green";
+   });
+
+
+red1.addEventListener("click",function(color){
+   
+    text.style.color = "red";
+   });
+
+
+
+blue1.addEventListener("click",function(color){
+   
+    text.style.color = "blue";
+   });
+   
